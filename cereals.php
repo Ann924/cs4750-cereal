0aeb1923c73ca657ac54f8163c21d8c7429b6974
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2023-04-12 02:53:38 -0400

    Styling

--- cereals.php
+++ cereals.php
@@ -81,6 +81,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Cereals</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
 
 <body>
@@ -165,20 +166,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     <div> Serving size:
                                         <?php echo get_cereal_nutrition($cereal['cereal_id'])['serving_size'] ?> oz.
                                     </div>
-                                    <div>
-                                        Upvotes: <?php echo get_cereal_upvotes($cereal['cereal_id'])['upvote_cnt'] ?>
-                                        <form action="cereals.php" method="post">
-                                            <input type="hidden" name="vote_cereal_id" value="<?php echo $cereal['cereal_id'] ?>"/>
-                                            <input type="submit" name="upvoteBtn" value="Upvote"/>
-                                        </form>
-                                    </div>
-                                    <div>
-                                        Downvotes: <?php echo get_cereal_downvotes($cereal['cereal_id'])['downvote_cnt'] ?>
-                                        <form action="cereals.php" method="post">
-                                            <input type="hidden" name="vote_cereal_id" value="<?php echo $cereal['cereal_id'] ?>"/>
-                                            <input type="submit" name="downvoteBtn" value="Downvote"/>
-                                        </form>
-                                    </div>
+                                </div>
+                                <div class="row justify-content-end">
+                                    <form action="cereals.php" method="post">
+                                        <input type="hidden" name="vote_cereal_id" value="<?php echo $cereal['cereal_id'] ?>"/>
+                                        <button type="submit" name="upvoteBtn" value="Upvote" class="btn">
+                                            <i class="far fa-thumbs-up"></i>
+                                        </button>
+                                        <?php echo get_cereal_upvotes($cereal['cereal_id'])['upvote_cnt'] ?>
+                                    </form>
+
+                                    <form action="cereals.php" method="post">
+                                        <input type="hidden" name="vote_cereal_id" value="<?php echo $cereal['cereal_id'] ?>"/>
+                                        <button type="submit" name="downvoteBtn" value="Downvote" class="btn">
+                                            <i class="far fa-thumbs-down"></i>
+                                        </button>
+                                        <?php echo get_cereal_downvotes($cereal['cereal_id'])['downvote_cnt'] ?>
+                                    </form>
                                 </div>
                             </div>
                         </div>
